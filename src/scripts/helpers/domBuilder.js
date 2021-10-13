8951fc98687b625ae9ca9f62fe657f8e7d17f7a1
Halie Dyer <Halie.L.Dyer@gmail.com>
2021-10-13 11:02:46 -0500

    refactored for cleaner code

--- src/scripts/helpers/domBuilder.js
+++
@@ -1,9 +0,0 @@
-const domBuilder = () => {
-  document.querySelector('#app').innerHTML = `
-   <div id="jokesContainer"></div>
-   <div id="punchlineContainer"></div>
-   <div id="buttonsContainer"></div>
-   `;
-};
-
-export default domBuilder;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,39 @@
-import 'bootstrap'; // import bootstrap elements and js
+import 'bootstrap';
 import '../styles/main.scss';
-import startApp from './helpers/startApp';
+import jokeData from './helpers/jokeData';
+import { getAnotherJokeButton, getPunchlineButton } from './helpers/components/buttons';
 
 const init = () => {
   document.querySelector('#app').innerHTML = `
+  <div id="mainContainer">
+  <button type="button" class="btn btn-outline-primary" id="getAJoke">Get A Joke!</button>
+  </div>
   `;
-  startApp();
+
+  let joke = {};
+
+  const showJoke = (obj) => {
+    joke = {
+      setup: obj.setup,
+      punchline: obj.delivery
+    };
+  };
+
+  document.querySelector('#mainContainer').addEventListener('click', (e) => {
+    if (e.target.id === 'getAJoke' || e.target.id === 'getAnotherJoke') {
+      jokeData().then((response) => {
+        showJoke(response);
+        document.querySelector('#mainContainer').innerHTML = `<h1>${joke.setup}</h1>`;
+        getPunchlineButton();
+      });
+    }
+
+    if (e.target.id === 'getPunchline') {
+      document.querySelector('#mainContainer').innerHTML = `<h1>${joke.setup}</h1>`;
+      document.querySelector('#mainContainer').innerHTML += `<p>${joke.punchline}</p>`;
+      getAnotherJokeButton();
+    }
+  });
 };
 
 init();
